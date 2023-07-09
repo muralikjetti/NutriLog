b09322afc738fbe1e5c3579c137a275c24205b00
Murali Jetti <68317934+muralikjetti@users.noreply.github.com>
2023-07-08 23:07:06 -0700

    Delete routes.py

--- website/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, render_template
-
-routes = Blueprint('routes', __name__)
-
-@routes.route('/', methods = ['GET','POST'])
-def home():
-    if request.method == 'GET':
-        return render_template('login.html')
